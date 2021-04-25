371128594d0484735ade0cc46931577c87aeb8a8
totori <49979579+totori0908@users.noreply.github.com>
2021-04-25 14:29:25 +0900

    Update result.js

--- result.js
+++ result.js
@@ -1,5 +1,26 @@
 const url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&applicationId=1064191778943027353';
 
+const item = '大根'
+const fs = require('fs');
+
+const jsonObject = JSON.parse(fs.readFileSync('./category.json', 'utf8'));
+console.log(jsonObject)
+
+const cate = ["large","medium","small"]
+
+id = ""
+for (var i in cate){
+    for(var menu in jsonObject["result"][cate[i]]){
+        if(jsonObject["result"][cate[i]][menu]["categoryName"] == item){
+            if(i == 0)id = jsonObject["result"][cate[i]][menu]['categoryId']
+            else{
+                if(id.length!=0) id += '-'
+                if(id.length == 0) id += jsonObject["result"][cate[i]][menu]['parentCategoryId'] + '-'
+                id += jsonObject["result"][cate[i]][menu]['categoryId']
+            }
+        }
+    }
+}
 
 fetch(url)
     .then(function (response1) { //成功時に実行される
